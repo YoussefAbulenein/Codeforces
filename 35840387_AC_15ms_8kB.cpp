ab29e58cc759e674ce296e595289095d228bbe4c
Youssef Abulenein <63174805+YoussefAbulenein@users.noreply.github.com>
2022-08-13 12:04:02 +0200

    Add files via upload

---
+++ 35840387_AC_15ms_8kB.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+//
+int main()
+{
+int n,s=0;
+cin >>n;
+
+int arr[n];
+
+for(int i=0;i<n;i++){
+
+
+    cin>>arr[i];
+
+if(arr[i]==1){
+    s++;
+
+}
+}
+cout<<s<<endl;
+for(int i=0;i<n;i++){
+
+    if(arr[i]>=arr[i+1]||i==n-1)
+   {
+
+    cout<<arr[i]<<" ";
+   }
+}
+
+
+
+    return 0;
+}
