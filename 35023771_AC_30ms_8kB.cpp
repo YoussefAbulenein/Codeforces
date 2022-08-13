4b67536172b0b7fe62a25bd8aca76278e333048f
Youssef Abulenein <63174805+YoussefAbulenein@users.noreply.github.com>
2022-08-13 12:00:32 +0200

    Add files via upload

---
+++ 35023771_AC_30ms_8kB.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int a, b,c=0,i;
+        cin >> a>>b;
+do
+            {c++;
+            a=a*3;
+            b=b*2;
+
+
+        }        while(a<=b);
+
+        cout <<c;
+    return 0;
+}
