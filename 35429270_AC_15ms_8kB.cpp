449e6aa51d2466665191deff1faae498fd67ae35
Youssef Abulenein <63174805+YoussefAbulenein@users.noreply.github.com>
2022-08-13 11:38:20 +0200

    Add files via upload

---
+++ 35429270_AC_15ms_8kB.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void cubes(){
+
+ int n,i=0;
+    cin>>n;
+    int arr[n];
+   for(int i = 0; i < n; i++){
+
+        cin>>arr[i];
+
+    }
+    sort(arr,arr+n);
+    i=0;
+
+for(int i = 0; i < n; i++){
+cout <<arr[i]<<" ";
+
+
+
+
+
+
+}
+
+
+
+}
+
+
+int main()
+{
+
+
+
+cubes();
+
+return 0;
+
+
+
+
+}
