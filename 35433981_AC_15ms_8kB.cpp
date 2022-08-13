f6a3e6385ebb323ca08a7ed9bc1d0288cd94f252
Youssef Abulenein <63174805+YoussefAbulenein@users.noreply.github.com>
2022-08-13 11:53:58 +0200

    Add files via upload

---
+++ 35433981_AC_15ms_8kB.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+#include <algorithm>
+using namespace std;
+ 
+ 
+int main () {
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int x;
+        cin >> x;
+        if(x == 1)
+        {
+            cout << -1;
+            return 0;
+        }
+    }
+    cout << 1;
+}
